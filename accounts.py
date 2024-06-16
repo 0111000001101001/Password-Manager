2da5d8378c7684896e10bd1b9172a5f4cb21df49
0111000001101001 <170758324+0111000001101001@users.noreply.github.com>
2024-06-16 12:05:23 -0400

    Delete accounts.py

--- accounts.py
+++
@@ -1,8 +0,0 @@
-def validate_username(username):
-     # Checks to see if the inputted username is comprised of only letters and numbers, 
-    # and has a minimum length of 3 characters and a maximum of 16.
-    return username.isalnum() and 3 <= len(username) <= 16
-
-def validate_password(password):
-    # Password must be at least 8 characters, and have max character count of 128.
-    return 8 <= len(password) <= 128
