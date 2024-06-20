4187f6e3d04f7f45bad9709d71c13604532858e3
0111000001101001 <170758324+0111000001101001@users.noreply.github.com>
2024-06-20 14:08:36 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from database_utils import create_master_accounts_db, create_password_manager_db, close_master_accounts_db_and_exit
+import sys
+from database_utils import init_master_accounts_db, init_password_manager_db, close_master_accounts_db_and_exit
 from account_operations import create_master_account, authenticate_log_in
 from crypto_utils import generate_fernet_key
 from menu import menu_options
@@ -19,13 +20,13 @@ Quit program (q)
 
 : """).lower().strip()
     
-    create_master_accounts_db()
+    init_master_accounts_db()
 
     while True:        
         if account_menu_input == '1':
             current_user = create_master_account()
             if current_user:
-                create_password_manager_db(current_user)
+                init_password_manager_db(current_user)
                 generate_fernet_key(current_user)
                 break
             else:
@@ -34,7 +35,7 @@ Quit program (q)
         elif account_menu_input == '2':
             current_user = authenticate_log_in()
             if current_user:
-                create_password_manager_db(current_user)
+                init_password_manager_db(current_user)
                 break
             else:
                 print("Too many failed attempts.")
@@ -45,10 +46,13 @@ Quit program (q)
         
         else:
             account_menu_input = input("\nInvalid input, try again.\n: ").lower().strip()
-            continue
 
     while True:
         menu_options(current_user)
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except Exception as e:
+        print(f"\nAn error has occured: {e}")
+        sys.exit(1)
